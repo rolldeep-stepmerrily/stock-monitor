b22534cab415037f5eceb93587b76b95699833ef
rolldeep-stepmerrily <rolldeep@stepmerrily.com>
2024-08-26 15:09:46 +0900

    update : basic setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
+
+import { ConfigProviderModule } from './common/config-provider/config-provider.module';
+import { HttpLoggerMiddleware } from './common/middlewares';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().required(),
+        PORT: Joi.number().default(3068),
+      }),
+      envFilePath: '.env',
+      isGlobal: true,
+      validationOptions: { abortEarly: true },
+    }),
+    ConfigProviderModule,
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(HttpLoggerMiddleware).forRoutes('*');
+  }
+}
